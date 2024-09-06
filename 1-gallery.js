fe653f10df9c2a5a56d6faa0f247a32c1c5cd0aa
AnaChaplyhina <167329280+AnaChaplyhina@users.noreply.github.com>
2024-09-06 11:59:45 +0000

    Deploying to gh-pages from @ AnaChaplyhina/goit-js-hw-09@0e3a4c878689962b90439bd65759b174b06e4b81 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,2 @@
-import"./assets/styles-B5Qt9EMX.js";console.log("Gallery");
+import"./assets/styles-B5Qt9EMX.js";import{S as o}from"./assets/vendor-CgTBfC_f.js";new o(".gallery a",{});console.log(o);
 //# sourceMappingURL=1-gallery.js.map
